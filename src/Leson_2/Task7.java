6e71c2bc4d40c1759280326a562c677ab798ea56
dytni <141308843+dytni@users.noreply.github.com>
2023-08-18 18:17:56 +0300

    task7

---
+++ src/Leson_2/Task7.java
@@ -0,0 +1,11 @@
+package Leson_2;
+
+public class Task7 {/*. Необходимо вывести на экран числа от 5 до 1.При решении
+используйте операцию декремента (--).*/
+
+    public static void main(String[] args) {
+        for(int i =5;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
