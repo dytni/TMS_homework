93380864b7e90b6fa8f014088aba0527a43c01ba
dytni <141308843+dytni@users.noreply.github.com>
2023-08-18 19:25:31 +0300

    task14

---
+++ src/Leson_2/Task14.java
@@ -0,0 +1,15 @@
+package Leson_2;
+
+public class Task14 {/*Напишите программу, которая выводит на консоль таблицу
+    умножения.*/
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("|%d*%d=%d\t",i,j,i*j);
+                //System.out.print("| " +i+"*"+j+"="+i*j );
+            }
+            System.out.println();
+        }
+    }
+}
