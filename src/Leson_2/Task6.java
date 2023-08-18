50cf90779c9da22702feea74144fdd1a1b2d73a7
dytni <141308843+dytni@users.noreply.github.com>
2023-08-18 18:13:20 +0300

    task6

---
+++ src/Leson_2/Task6.java
@@ -0,0 +1,11 @@
+package Leson_2;
+
+public class Task6 {/*1. При помощи цикла for вывести на экран нечетные числа от 1 до
+99.  При решении используйте операцию инкремента (++).*/
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
