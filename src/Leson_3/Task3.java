488d381d89cc340de049c1a2145f6f74c7f45541
dytni <nikolajolesko391@gmail.com>
2023-08-22 20:54:06 +0300

    yo

--- src/Leson_3/Task3.java
+++ src/Leson_3/Task3.java
@@ -8,14 +8,28 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         int size;
+        double max;
+        double min;
+        double average;
+        double sum=0;
+        System.out.println("Enter size of array: ");
         Scanner input = new Scanner(System.in);
         size = input.nextInt();
         double []mas= new double[size];
         double randomNum;
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < size; i++) {
             randomNum = Math.random();
             mas[i] = randomNum;
             System.out.println(mas[i]);
         }
+        min=mas[0];
+        max=mas[0];
+        for(double num: mas){
+            sum=sum+num;
+            if(num>max)max=num;
+            if(num<min)min=num;
+        }
+        average=sum/size;
+        System.out.println("Minimum = "+min+" Maximum = "+max+" Average = "+ average);
     }
 }
