3f8d6f2f061fe7e41307a7140f682f220d603a1e
dytni <nikolajolesko391@gmail.com>
2023-08-24 02:22:25 +0300

    Task 5

--- src/Leson_3/Task1.java
+++ src/Leson_3/Task1.java
@@ -7,15 +7,12 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random ran = new Random();
-        int max = 50;
-        int min = 0;
-        int range = max - min + 1;
         System.out.println("Enter key:");
         int key = in.nextInt();
         int[] mas = new int[15];
         for (int i = 0; i < 15; i++) {
-            int a = ran.nextInt(range) + min;
-            mas[i] = a;
+
+            mas[i] = ran.nextInt(51);
             System.out.println(mas[i]);
             if (mas[i] == key) {
                 System.out.println("Key was finded");
