f12cf354075d408d648825ef80b705aca450d28a
dytni <nikolajolesko391@gmail.com>
2023-08-29 20:35:41 +0300

    Leson 5 Task 1

---
+++ src/Leson_5/CreditCard.java
@@ -0,0 +1,20 @@
+package Leson_5;
+
+public class CreditCard {
+
+    int number_of_card;
+    int balance;
+
+    void add_cash(int sum) {
+        balance = balance + sum;
+    }
+
+    void take_cash(int sum) {
+        balance = balance - sum;
+    }
+
+    void show_info() {
+        System.out.println("Card number " + number_of_card + " Balance = " + balance);
+    }
+
+}
