6c992af49dffaa6e907c3f5079ad358a093c10ad
dytni <nikolajolesko391@gmail.com>
2023-09-12 20:52:25 +0300

    trash....

--- src/Lesson_7/Lesson_7_Task_1/Lesson_7_task_1.java
+++ src/Lesson_7/Lesson_7_Task_1/Lesson_7_task_1.java
@@ -4,13 +4,13 @@ public class Lesson_7_task_1 {
      public static void main(String[] args) {
          Phone phone_1=new Phone(802985656,209, "iphone");
          Phone phone_2=new Phone(802911111, 315, "pocofon x3000 powerX limited edition");
-         Phone phone_3=new Phone(802900000, 596, "nokia");
+         Phone phone_3=new Phone(802900000, "nokia");
          phone_1.receiveCall("ваня");
-         System.out.println("number: "+phone_1.getNumber());
-         phone_2.receiveCall("олег");
-         System.out.println("number: "+phone_2.getNumber());
-         phone_3.receiveCall("артур");
          System.out.println("number: "+phone_3.getNumber());
+         phone_2.receiveCall("олег", phone_1);
+         phone_3.receiveCall("артур", phone_2);
+         System.out.println();
+         phone_1.sendMassage("hi",phone_2,phone_3);
 
      }
 }
--- src/Lesson_7/Lesson_7_Task_1/Phone.java
+++ src/Lesson_7/Lesson_7_Task_1/Phone.java
@@ -1,18 +1,36 @@
 package Lesson_7.Lesson_7_Task_1;
 
 public class Phone {
-    private final int number;
-    private final float weight;
-    private final String model;
-    Phone(int number, float weight, String model){
+    private int number;
+    private float weight;
+    private String model;
+    Phone(){
+        number=0;
+        weight=0;
+        model=" ";
+    }
+    Phone(int number, String model){
         this.number=number;
-        this.weight= weight;
         this.model=model;
+        weight=0;
+    }
+    Phone(int number, float weight, String model){
+         this(number,model);
+         this.weight=weight;
     }
     public int getNumber() {
         return number;
     }
     void receiveCall(String name){
         System.out.println("Enter call "+ name);
+    }
+    void receiveCall(String name, Phone phone){
+        System.out.println("Enter call "+ name);
+        System.out.println("number: "+phone.getNumber());
+    }
+    void sendMassage(String massage, Phone... phones){
+        for (Phone phone: phones){
+            System.out.println("on number: "+phone.getNumber()+" send massage: "+ massage);
+        }
     }
 }
