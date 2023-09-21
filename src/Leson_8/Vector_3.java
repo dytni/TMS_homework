644d3a0f8cb1a92eb935f6e36d77d5097384ece8
dytni <nikolajolesko391@gmail.com>
2023-09-21 19:54:15 +0300

    trash....

---
+++ src/Leson_8/Vector_3.java
@@ -0,0 +1,58 @@
+package Leson_8;
+
+import java.util.Random;
+
+public class Vector_3 {
+    int x;
+    int y;
+    int z;
+    final String info = " This is tree-dimensional vector";
+
+    Vector_3(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    double vector_len() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    int vector_scalar(Vector_3 vector_3) {
+        if (vector_3 == null) throw new IllegalArgumentException("Error argument");
+        return x * vector_3.x + y * vector_3.y + z * vector_3.z;
+    }
+
+    Vector_3 vector_sum(Vector_3 vector_3) {
+        if (vector_3 == null) throw new IllegalArgumentException("Error argument");
+        return new Vector_3(x + vector_3.x,
+                y + vector_3.y, z + vector_3.z);
+    }
+
+    Vector_3 vector_diff(Vector_3 vector_3) {
+        if (vector_3 == null) throw new IllegalArgumentException("Error operator");
+        return new Vector_3(x - vector_3.x, y - vector_3.y, z - vector_3.z);
+    }
+
+    static Vector_3[] vector_3s(int n) {
+        if (n < 0) throw new IllegalArgumentException("Error operator");
+        Random random = new Random();
+        Vector_3[] vector_3s = new Vector_3[n];
+        for (int i = 0; i < n; i++) {
+            vector_3s[i] = new Vector_3(random.nextInt(16), random.nextInt(16), random.nextInt(16));
+        }
+        return vector_3s;
+    }
+
+    Boolean vector_is_longer(Vector_3 vector_3) {
+        if (vector_3 == null) throw new IllegalArgumentException("Error operator");
+        return this.vector_len() < vector_3.vector_len();
+    }
+
+    void vector_show_info() {
+        System.out.println(info);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+}
---
+++ src/Leson_8/Vector_arr.java
@@ -0,0 +1,76 @@
+package Leson_8;
+
+import java.util.Random;
+
+public class Vector_arr {
+    int[] coordinates;
+
+    Vector_arr(int[] arr) {
+        coordinates = arr;
+    }
+
+    double vector_len() {
+        int sum = 0;
+        for (int coordinate : this.coordinates) {
+            sum += coordinate * coordinate;
+        }
+        return Math.sqrt(sum);
+    }
+
+    int vector_scalar(Vector_arr vector_arr) {
+        if (vector_arr == null || vector_arr.coordinates.length != this.coordinates.length)
+            throw new IllegalArgumentException("Error argument");
+        int sum = 0;
+        for (int i = 0; i < coordinates.length; i++) {
+            sum += coordinates[i] * vector_arr.coordinates[i];
+        }
+        return sum;
+    }
+
+    Vector_arr vector_sum(Vector_arr vector_arr) {
+        if (vector_arr == null || vector_arr.coordinates.length != this.coordinates.length)
+            throw new IllegalArgumentException("Error argument");
+        int[] sum = new int[coordinates.length];
+        for (int i = 0; i < coordinates.length; i++) {
+            sum[i] = coordinates[i] + vector_arr.coordinates[i];
+        }
+        return new Vector_arr(sum);
+    }
+
+    Vector_arr vector_diff(Vector_arr vector_arr) {
+        if (vector_arr == null || vector_arr.coordinates.length != this.coordinates.length)
+            throw new IllegalArgumentException("Error operator");
+        int[] dif = new int[coordinates.length];
+        for (int i = 0; i < coordinates.length; i++) {
+            dif[i] = coordinates[i] - vector_arr.coordinates[i];
+        }
+        return new Vector_arr(dif);
+    }
+
+    static Vector_arr[] vector_arr_s(int n, int coordinates_amount) {
+        if (n < 0 || coordinates_amount < 0) throw new IllegalArgumentException("Error operator");
+        Random random = new Random();
+        Vector_arr[] vector_arr_s = new Vector_arr[n];
+        for (int i = 0; i < n; i++) {
+            int[] new_coordinates = new int[coordinates_amount];
+            for (int j = 0; j < coordinates_amount; j++) {
+                new_coordinates[j] = random.nextInt(16);
+            }
+            vector_arr_s[i] = new Vector_arr(new_coordinates);
+        }
+        return vector_arr_s;
+    }
+
+    Boolean vector_is_longer(Vector_arr vector_arr) {
+        if (vector_arr == null || vector_arr.coordinates.length != this.coordinates.length)
+            throw new IllegalArgumentException("Error operator");
+        return this.vector_len() < vector_arr.vector_len();
+    }
+
+    void vector_show_info() {
+        System.out.println("This is " + coordinates.length +"-dimensional vector");
+        for (int i = 0; i < coordinates.length; i++) {
+            System.out.println("Coordinate " + (i+1) + " = " + coordinates[i]);
+        }
+    }
+}
