4e93079c7fd19f3589f3715dab1d853768f5d4f1
dytni <nikolajolesko391@gmail.com>
2023-09-22 13:06:44 +0300

    trash....

---
+++ src/Lesson_9/Treatment_string.java
@@ -0,0 +1,47 @@
+package Lesson_9;
+
+public class Treatment_string {
+    static void show_2_blocks_with_numbers(String string) {
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        String[] string1 = string.split("-");
+        System.out.println(string1[0] + string1[2]);
+    }
+
+    static void show_string_with_blur(String string) {
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        StringBuffer stringBuffer = new StringBuffer(string);
+        System.out.println(stringBuffer.replace(5, 8, "***").replace(14, 17, "***"));
+
+    }
+
+    static void show_letters_toLower_case(String string) {
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        String[] string1 = string.split("-");
+        System.out.print(string1[1].toLowerCase() + "/" + string1[3].toLowerCase() + "/");
+        System.out.println(string1[4].toLowerCase().charAt(1) + "/" + string1[4].toLowerCase().charAt(3));
+    }
+
+    static void show_letters_toUpper_case(String string) {
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        String[] string1 = string.split("-");
+        System.out.print(string1[1].toUpperCase() + "/" + string1[3].toUpperCase() + "/");
+        StringBuilder stringBuilder = new StringBuilder(string1[4].toUpperCase());
+        System.out.println(stringBuilder.charAt(1) + "/" + stringBuilder.charAt(3));
+    }
+
+    static void is_string_contain_abc(String string) {
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        if(string.contains("abc")||string.contains("ABC"))System.out.println("Contain");
+        else System.out.println("Does not contain");
+    }
+    static void is_string_starts_from_555(String string){
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        if(string.startsWith("555"))System.out.println("Yes");
+        else System.out.println("No");
+    }
+    static void is_string_ends_with_1a2b(String string){
+        if (string == null) throw new IllegalArgumentException("Error argument");
+        if(string.endsWith("1a2b"))System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
