9c02407c19b1e3f55061b26a76c4bce84c6025a7
dytni <nikolajolesko391@gmail.com>
2023-09-24 22:08:11 +0300

    additional task first method

--- src/Leson_3/Task1.java
+++ src/Leson_3/Task1.java
@@ -8,15 +8,22 @@ public class Task1 {
         Scanner in = new Scanner(System.in);
         Random ran = new Random();
         System.out.println("Enter key:");
+        boolean isExist =false;
         int key = in.nextInt();
         int[] mas = new int[15];
         for (int i = 0; i < 15; i++) {
             mas[i] = ran.nextInt(51);
             System.out.println(mas[i]);
-            if (mas[i] == key) {
-                System.out.println("Key was finded");
+        }
+        for(int element:mas){
+            if(element==key){
+                isExist=true;
                 break;
             }
         }
+        if (isExist){
+            System.out.println("key was found");
+        }
+        else System.out.println("key wasn't found");
     }
 }
