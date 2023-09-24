db82bc967510d76c91e0134cdd87eeabe5ce3598
dytni <nikolajolesko391@gmail.com>
2023-09-24 21:20:41 +0300

    additional task first method

--- src/Leson_2/Task7.java
+++ src/Leson_2/Task7.java
@@ -1,10 +1,9 @@
 package Leson_2;
-
-public class Task7 {/*. Необходимо вывести на экран числа от 5 до 1.При решении
+/*. Необходимо вывести на экран числа от 5 до 1.При решении
 используйте операцию декремента (--).*/
-
+public class Task7 {
     public static void main(String[] args) {
-        for(int i =5;i>0;i--){
+        for (int i = 5; i > 0; i--) {
             System.out.println(i);
         }
     }
