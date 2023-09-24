4f5292d9d5f1e230d366cb0b5e5c2c7517d0bfb2
dytni <nikolajolesko391@gmail.com>
2023-09-24 21:00:34 +0300

    additional task first method

--- src/Leson_2/Task3.java
+++ src/Leson_2/Task3.java
@@ -1,16 +1,15 @@
 package Leson_2;
 
 import java.util.Scanner;
-
-public class Task3 {/*Напишите программу, которая будет принимать на вход
+/*Напишите программу, которая будет принимать на вход
 число и на выход будет выводить сообщение четное число или нет.
 Для определения четности числа используйте операцию получения
 остатка от деления - операция выглядит так: '% 2'*/
+public class Task3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number;
         System.out.println("Enter number");
-        number = input.nextInt();
+        int number = input.nextInt();
         if (number % 2 == 0) {
             System.out.println("Even number");
         } else {
