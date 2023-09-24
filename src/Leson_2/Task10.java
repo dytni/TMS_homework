7b9dadc6d53a07113b7a4adeab414db171a605ca
dytni <nikolajolesko391@gmail.com>
2023-09-24 21:27:15 +0300

    additional task first method

--- src/Leson_2/Task10.java
+++ src/Leson_2/Task10.java
@@ -1,12 +1,10 @@
 package Leson_2;
-
-public class Task10 {/*5. Вывести 10 первых чисел последовательности 0, -5,-10,-15..
-*/
+/*5. Вывести 10 первых чисел последовательности 0, -5,-10,-15...
+ */
+public class Task10 {
     public static void main(String[] args) {
-        int sum=0;
         for (int i = 0; i < 11; i++){
-            System.out.println(sum);
-            sum = sum - 5;
+            System.out.println(i*-5);
         }
     }
 }
