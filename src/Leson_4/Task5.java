b44ddbd97ea1f9b106da5e92a00c8d9e707fae0e
dytni <nikolajolesko391@gmail.com>
2023-09-24 23:08:54 +0300

    additional task first method

--- src/Leson_4/Task5.java
+++ src/Leson_4/Task5.java
@@ -1,25 +1,29 @@
 package Leson_4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /*Создайте двумерный массив. Выведите на консоль диагонали массива.*/
 public class Task5 {
      public static void main(String[] args) {
           Random random = new Random();
-          int[][] arr = new int[3][3];
-          for (int i = 0; i < 3; i++) {
-               for (int j = 0; j < 3; j++) {
+          Scanner scanner =  new Scanner(System.in);
+          System.out.println("Enter size of matrix");
+          int size = scanner.nextInt();
+          int[][] arr = new int[size][size];
+          for (int i = 0; i < size; i++) {
+               for (int j = 0; j < size; j++) {
                     arr[i][j] = random.nextInt(11);
                }
           }
           System.out.println("first diagonal");
-          for(int i=0;i<3;i++){
+          for(int i=0;i<size;i++){
                System.out.print(" "+  arr[i][i]);
           }
           System.out.println();
           System.out.println("second diagonal");
-          for(int i=0;i<3;i++){
-               System.out.print(" "+  arr[2-i][i]);
+          for(int i=0;i<size;i++){
+               System.out.print(" "+  arr[size-1-i][i]);
           }
 
      }
