7491f7359208419d62dd52e85853c46c4dbb021a
dytni <141308843+dytni@users.noreply.github.com>
2023-09-24 21:13:34 +0300

    Update Task5.java

--- src/Leson_2/Task5.java
+++ src/Leson_2/Task5.java
@@ -1,15 +1,14 @@
 package Leson_2;
 
 import java.util.Scanner;
-
-public class Task5 {/*. По введенному номеру определить цвет радуги (1 –
+/*. По введенному номеру определить цвет радуги (1 –
 красный, 4 – зеленый и т. д.).*/
+public class Task5 {
     public static void main(String[] args) {
         String[] arr = new String[]{"красный", "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый"};
-        int number;
         System.out.println("Enter number");
         Scanner input = new Scanner(System.in);
-        number = input.nextInt();
+        int number = input.nextInt();
         System.out.println(arr[number - 1]);
     }
 }
