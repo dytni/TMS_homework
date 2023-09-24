f9e5c90cbe412aa4deb8c22bcf82a95aa4437fcd
dytni <nikolajolesko391@gmail.com>
2023-09-24 21:24:13 +0300

    additional task first method

--- src/Leson_2/Task9.java
+++ src/Leson_2/Task9.java
@@ -1,15 +1,14 @@
 package Leson_2;
-
-public class Task9 {/*Необходимо, чтоб программа выводила на экран вот такую
+/*Необходимо, чтоб программа выводила на экран вот такую
 последовательность: 7 14 21 28 35 42 49 56 63 70 77 84 91 98. В решении
 используйте цикл while.
 */
-
+public class Task9 {
     public static void main(String[] args) {
-        int i = 7;
-        while (i < 99) {
-            System.out.println(i);
-            i = i + 7;
+        int element = 7;
+        while (element < 99) {
+            System.out.println(element);
+            element = element + 7;
         }
     }
 }
