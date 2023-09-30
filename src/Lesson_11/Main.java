a3130946e3e2b3081c21bd137a1b69572e7d89fd
dytni <nikolajolesko391@gmail.com>
2023-09-30 18:26:28 +0300

    1 task, 2 and files

---
+++ src/Lesson_11/Main.java
@@ -0,0 +1,14 @@
+package Lesson_11;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter path to file");
+        String path = scanner.next();
+        //path = "src//Lesson_11//Documents.txt";
+        FirstTask.fileCheck(path);
+        SecondTask.fileCheck(path);
+    }
+}
