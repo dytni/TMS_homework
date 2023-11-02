90eb3f465f15a49670ca6822a5c391de06e6c837
dytni <nikolajolesko391@gmail.com>
2023-11-02 20:25:38 +0300

    1 task

--- src/Lesson_5/Lesson_5_Task_3/ATM.java
+++ src/Lesson_5/Lesson_5_Task_3/ATM.java
@@ -17,7 +17,6 @@ public class ATM {
     }
 
     boolean takeMoney(int sum) {
-        Money money = new Money();
         int count100 = 0;
         int count50 = 0;
         int count20 = 0;
@@ -25,17 +24,17 @@ public class ATM {
             sum = sum - 100;
             count100 += 1;
         }
-        money.setCount(count100, Denomination.DOLLAR_100);
+        atmMoney.setCount(atmMoney.getCount(Denomination.DOLLAR_100) - count100, Denomination.DOLLAR_100);
         while (sum >= 90) {
             sum = sum - 50;
             count50 += 1;
         }
-        money.setCount(count50, Denomination.DOLLAR_50);
+        atmMoney.setCount(atmMoney.getCount(Denomination.DOLLAR_50) - count50, Denomination.DOLLAR_50);
         while (sum != 0) {
             sum = sum - 20;
             count20 += 1;
         }
-        money.setCount(count20, Denomination.DOLLAR_20);
+        atmMoney.setCount(atmMoney.getCount(Denomination.DOLLAR_20) - count20, Denomination.DOLLAR_20);
         System.out.println(" You will have \n"+ count20 +" banknotes with denomination 20\n"
                 + count50 +" banknotes with denomination 50\n"
                 + count100 +" banknotes with denomination 100\n");
