03b7d31a003b862a1783555c6be70a345e3ffdb6
dytni <nikolajolesko391@gmail.com>
2023-11-05 14:47:33 +0300

    1 task

--- src/Lesson_7/Lesson_7_Task_2/TreatmentPlan.java
+++ src/Lesson_7/Lesson_7_Task_2/TreatmentPlan.java
@@ -1,14 +1,20 @@
 package Lesson_7.Lesson_7_Task_2;
 
 public class TreatmentPlan {
-    void addPlan(int kod){
-        if(kod==1){
+    Doctors doctors;
+
+    TreatmentPlan(Doctors doctors) {
+        this.doctors = doctors;
+    }
+
+    void addPlan() {
+        if (doctors == Doctors.SURGEON) {
             Surgeon surgeon = new Surgeon();
             surgeon.heal();
-        } else if (kod==2) {
-            Dentist dentist= new Dentist();
+        } else if (doctors == Doctors.DENTIST) {
+            Dentist dentist = new Dentist();
             dentist.heal();
-        }else {
+        } else {
             Therapist therapist = new Therapist();
             therapist.heal();
         }
